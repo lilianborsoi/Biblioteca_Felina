c18956c67f1297cc6ac56bca2f2040625c528c25
lilian borsoi <lilianborsoi@icloud.com>
2024-09-07 21:02:03 -0300

    Adicionado objetos e valores

---
+++ dados.js
@@ -0,0 +1,85 @@
+let dados = [
+    
+    {
+        raça: 'Siamês',
+        caracteristicas: 'Inteligente, curioso e muito vocal.',
+        pelagem: 'Curta e sedosa',
+        expectativaDeVida: 15,
+        paisDeOrigem: 'Tailândia',
+        link: 'https://www.petz.com.br/gato/racas/siames/'
+      },
+      {
+        raça: 'Persa',
+        caracteristicas: 'Calmo, gentil e com uma pelagem que requer muitos cuidados.',
+        pelagem: 'Longa e densa',
+        expectativaDeVida: 13,
+        paisDeOrigem: 'Irã',
+        link: 'https://www.petz.com.br/gato/racas/persa/'
+      },
+      {
+        raça: 'Abissínio',
+        caracteristicas: 'Ativo, ágil e com um instinto caçador bem desenvolvido.',
+        pelagem: 'Curta e brilhante',
+        expectativaDeVida: 15,
+        paisDeOrigem: 'Egito',
+        link: 'https://www.petz.com.br/gato/racas/abissinio/'
+      },
+      {
+        raça: 'British Shorthair',
+        caracteristicas: 'Robusto, calmo e com um rosto redondo característico.',
+        pelagem: 'Curta e densa',
+        expectativaDeVida: 15,
+        paisDeOrigem: 'Reino Unido',
+        link: 'https://www.petz.com.br/gato/racas/british-shorthair/'
+      },
+      {
+        raça: 'Sphynx',
+        caracteristicas: 'Inteligente, curioso e sem pelo.',
+        pelagem: 'Sem pelo',
+        expectativaDeVida: 14,
+        paisDeOrigem: 'Canadá',
+        link: 'https://www.petz.com.br/gato/racas/sphynx/'
+      },
+      {
+        raça: 'Ragdoll',
+        caracteristicas: 'Calmo, dócil e com uma pelagem macia e sedosa.',
+        pelagem: 'Longa e semi-longa',
+        expectativaDeVida: 15,
+        paisDeOrigem: 'Estados Unidos',
+        link: 'https://www.petz.com.br/gato/racas/ragdoll/'
+      },
+      {
+        raça: 'Exótico',
+        caracteristicas: 'Calmo, gentil e com uma pelagem densa e curta.',
+        pelagem: 'Curta e densa',
+        expectativaDeVida: 15,
+        paisDeOrigem: 'Estados Unidos',
+        link: 'https://www.petz.com.br/gato/racas/exotico/'
+      },
+      {
+        raça: 'Vira lata',
+        caracteristicas: 'Personalidade variada, resistente e adaptável.',
+        pelagem: 'Variada, dependendo da mistura genética.',
+        expectativaDeVida: '15 a 20',
+        paisDeOrigem: 'Mundial',
+        link: 'https://www.petz.com.br/gato/racas/gato-vira-lata/'
+      },
+      {
+        raça: 'Angorá',
+        caracteristicas: 'Inteligente, ativo e com uma pelagem longa e sedosa.',
+        pelagem: 'Longa e sedosa',
+        expectativaDeVida: '15 a 20 anos',
+        paisDeOrigem: 'Turquia',
+        link: 'https://www.petz.com.br/gato/racas/angora/'
+      },
+      {
+        raça: 'Himalaio',
+        caracteristicas:'Gatos de porte médio, conhecidos por seus olhos azuis intensos e pelagem longa e sedosa.',
+        pelagem: 'Longa, densa e sedosa, com cores que variam do branco ao marrom escuro, geralmente com pontos coloridos no rosto, orelhas, patas e cauda.',
+        expectativaDeVida: '15 a 20 ',
+        paisDeOrigem: 'Tailândia',
+        link: 'https://www.petz.com.br/gato/racas/himalaio/'
+      }
+      
+];
+
